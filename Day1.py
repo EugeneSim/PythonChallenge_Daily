59281fb40bae413009fd57bf5a3447c18df51c5b
EugeneSim <86097102+EugeneSim@users.noreply.github.com>
2024-03-10 14:25:24 +0800

    Line continuation character

---
+++ Day1.py
@@ -0,0 +1,8 @@
+print("I am learning Python and trying to make a discipline of coding daily")
+
+# print("I am learning Python and trying
+#       to make a discipline of coding daily")
+
+print("I am learning Python and trying \
+to make a discipline of coding daily")
+
